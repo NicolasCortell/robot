70b5c357523a5fb7e21ba9b554e0fcc6fa8758fa
Nicolas Cortell <nicolascortell@gmail.com>
2020-04-15 09:38:00 +0200

    SHITTY CSS RESPONSIVE

--- pages/common/front/_tpl_header.php
+++ pages/common/front/_tpl_header.php
@@ -20,47 +20,42 @@
     <body class="bg-grey"> 
         <!-- DESKTOP MENU -->
         <div class="row menu valign-wrapper bg-white hide-on-med-and-down">
-            <div class="col s1"></div>
-            <div class="col s1 brand-logo"><img src="resources/img/logo.svg"/></div>
-            <div class="col s1 menu-item txt-dark-blue valign-wrapper"><i class="material-icons-outlined prefix">home</i>Accueil</div>
-            <div class="col s1 menu-item txt-dark-blue valign-wrapper"><i class="material-icons prefix">list_alt</i>Recettes</div>
-            <div class="col s1 menu-item txt-dark-blue valign-wrapper"><span class="svg-icon"></span>Robots</div>
-            <div class="col s2"><a href="#"class="brand-logo-txt brand-logo txt-green">COMPANY NAME</a></div>
-            <div class="col s1 "></div>
-            <div class="col s2">
+            <div class="col l1"></div>
+            <div class="col l1 brand-logo"><img src="resources/img/logo.svg"/></div>
+            <div class="col l1 menu-item txt-dark-blue valign-wrapper" style="text-align:right;"><i class="material-icons-outlined prefix">home</i>Accueil</div>
+            <div class="col l1 menu-item txt-dark-blue valign-wrapper"><i class="material-icons prefix">list_alt</i>Recettes</div>
+            <div class="col l1 menu-item txt-dark-blue valign-wrapper"><i class="material-icons-outlined prefix">toys</i>Robots</div>
+            <div class="col l2 center-align"><a href="#"class="brand-logo-txt brand-logo txt-green">COMPANY</a></div>
+            <div class="col l1"></div>
+            <div class="col l2">
                 <div id="search" class="input-field txt-light-blue bg-grey">
                     <i class="material-icons prefix">search</i>
                     <input placeholder="Rechercher" type="text">
                 </div>
             </div>
-            <div class="col s1 menu-item txt-dark-blue">Connexion</div>
-            <div class="col s1"></div>
+            <div class="col l2 left-align menu-item txt-dark-blue">Connexion</div>
         </div>
         <!-- TABLET MENU -->
         <div class="row menu valign-wrapper bg-white hide-on-large-only hide-on-small-only">
-            <div class="col s1"></div>
-            <div class="col s1 menu-item txt-dark-blue valign-wrapper"><i class="material-icons-outlined prefix">home</i>Accueil</div>
-            <div class="col s1 menu-item txt-dark-blue valign-wrapper"><i class="material-icons prefix">list_alt</i>Recettes</div>
-            <div class="col s1 menu-item txt-dark-blue valign-wrapper"><span class="svg-icon"></span>Robots</div>
-            <div class="col s1"></div>
-            <div class="col s1 brand-logo"><img src="resources/img/logo.svg"/></div>
-            <div class="col s1"></div>
-            <div class="col s3">
+            <div class="col m5">
+                <div class="col m4 menu-item txt-dark-blue valign-wrapper"><i class="material-icons-outlined prefix">home</i>Accueil</div>
+                <div class="col m4 menu-item txt-dark-blue valign-wrapper"><i class="material-icons prefix">list_alt</i>Recettes</div>
+                <div class="col m4 menu-item txt-dark-blue valign-wrapper"><i class="material-icons-outlined prefix">toys</i>Robots</div>
+            </div>
+            <div class="col m2 center-align brand-logo"><img src="resources/img/logo.svg"/></div>
+            <div class="col m3">
                 <div id="search" class="input-field txt-light-blue bg-grey">
                     <i class="material-icons prefix">search</i>
                     <input placeholder="Rechercher" type="text">
                 </div>
             </div>
-            <div class="col s1 menu-item txt-dark-blue">Connexion</div>
-            <div class="col s1"></div>
+            <div class="col m2 right-align menu-item txt-dark-blue">Connexion</div>
         </div>
         <!-- MOBILE MENU -->
         <div class="row menu valign-wrapper bg-white hide-on-med-and-up">
-            <div class="col s1 "></div>
-            <div class="col s3 txt-dark-blue left-align"><i class="material-icons-outlined prefix">home</i></div>
+            <div class="col s4 menu-item txt-dark-blue center-align"><i class="material-icons-outlined prefix">toc</i></div>
             <div class="col s4 brand-logo center-align"><img src="resources/img/logo.svg"/></div>
-            <div class="col s3 txt-dark-blue right-align"><i class="material-icons-outlined prefix">home</i></div>
-            <div class="col s1 "></div>
+            <div class="col s4 menu-item txt-dark-blue center-align"><i class="material-icons-outlined prefix">perm_identity</i></div>
         </div>
 
         <div id="left-menu" class="hide-on-small-only txt-dark-blue">
